a9191bc333f43ea2c4f346428e1d29de1fca99f5
Konrad-Abramowski <abramowski.konrad@gmail.com>
2020-08-08 20:49:21 +0200

    Add GetMappings in IngredientController

--- src/main/java/com/example/cookingapp/controllers/IngredientController.java
+++ src/main/java/com/example/cookingapp/controllers/IngredientController.java
@@ -2,15 +2,18 @@ package com.example.cookingapp.controllers;
 
 import com.example.cookingapp.model.Ingredient;
 import com.example.cookingapp.repositories.IngredientRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
-@RequestMapping(name = "/ingredients")
+@RequestMapping("/ingredients")
 class IngredientController {
 
     private final IngredientRepository ingredientRepository;
@@ -25,4 +28,15 @@ class IngredientController {
         return ResponseEntity.created(URI.create("/" + result.getId())).body(result);
     }
 
+    @GetMapping
+    ResponseEntity<List<Ingredient>> readAllIngredients() {
+        return ResponseEntity.ok(ingredientRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    ResponseEntity<Ingredient> readIngredient(@PathVariable int id) {
+        return ingredientRepository.findById(id)
+                .map(ingredient -> ResponseEntity.ok(ingredient))
+                .orElse(ResponseEntity.notFound().build());
+    }
 }
