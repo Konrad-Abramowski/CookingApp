6015f7574e064df2cf82dca83b6327d234da05f3
Konrad-Abramowski <abramowski.konrad@gmail.com>
2020-08-09 17:23:12 +0200

    Add header on main page

--- src/main/java/com/example/cookingapp/CookingappApplication.java
+++ src/main/java/com/example/cookingapp/CookingappApplication.java
@@ -2,6 +2,8 @@ package com.example.cookingapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
 
 @SpringBootApplication
 public class CookingappApplication {
@@ -9,5 +11,4 @@ public class CookingappApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CookingappApplication.class, args);
 	}
-
 }
