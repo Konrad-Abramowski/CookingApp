bf5126a5846136d3d8b0e42447ea24bced907b63
Konrad-Abramowski <abramowski.konrad@gmail.com>
2020-08-12 16:03:42 +0200

    Add GetMapping methods in RecipeController

--- src/main/java/com/example/cookingapp/controllers/RecipeController.java
+++ src/main/java/com/example/cookingapp/controllers/RecipeController.java
@@ -1,9 +1,34 @@
 package com.example.cookingapp.controllers;
 
+import com.example.cookingapp.model.Recipe;
+import com.example.cookingapp.repositories.RecipeRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/recipes")
-public class RecipeController {
+class RecipeController {
+
+    private final RecipeRepository recipeRepository;
+
+    RecipeController(final RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    @GetMapping
+    ResponseEntity<List<Recipe>> readAllRecipes(){
+        return ResponseEntity.ok(recipeRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    ResponseEntity<Recipe> readRecipe(@PathVariable int id){
+        return recipeRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
 }
