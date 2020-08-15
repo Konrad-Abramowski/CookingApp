ed943ccafeffd9eefc756d55a5c1684d13267c7b
Konrad-Abramowski <abramowski.konrad@gmail.com>
2020-08-15 21:18:41 +0200

    Add addIngredientToRecipe method in IngredientInRecipeRepository

--- src/main/java/com/example/cookingapp/repositories/IngredientInRecipeRepository.java
+++ src/main/java/com/example/cookingapp/repositories/IngredientInRecipeRepository.java
@@ -2,9 +2,23 @@ package com.example.cookingapp.repositories;
 
 import com.example.cookingapp.model.IngredientInRecipe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 
 @Repository
 public interface IngredientInRecipeRepository extends JpaRepository<IngredientInRecipe, Integer> {
+
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true, value = "insert into INGREDIENT_IN_RECIPE(INGREDIENT_ID, RECIPE_ID, AMOUNT) " +
+            "values (:ingredient_id, :recipe_id, :amount)")
+    int addIngredientToRecipe(@Param("ingredient_id") int ingredient_id,
+                                          @Param("recipe_id") int recipe_id,
+                                          @Param("amount") String amount);
+
 
 }
