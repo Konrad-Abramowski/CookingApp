beb49af5863b36a313af3b7b61fec02965f29e07
Konrad-Abramowski <abramowski.konrad@gmail.com>
2020-08-17 14:53:23 +0200

    Make AmountRepository public

--- src/main/java/com/example/cookingapp/repositories/AmountRepository.java
+++ src/main/java/com/example/cookingapp/repositories/AmountRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-interface AmountRepository extends JpaRepository<Amount, Integer> {
+public interface AmountRepository extends JpaRepository<Amount, Integer> {
 }
