723391fa9e16968aeb9eb97de39917a1bffd9bac
Konrad-Abramowski <abramowski.konrad@gmail.com>
2020-08-17 16:34:13 +0200

    Add addIngredientToRecipe method

--- src/main/java/com/example/cookingapp/controllers/RecipeController.java
+++ src/main/java/com/example/cookingapp/controllers/RecipeController.java
@@ -1,11 +1,11 @@
 package com.example.cookingapp.controllers;
 
-import com.example.cookingapp.model.Ingredient;
-import com.example.cookingapp.model.IngredientInRecipe;
-import com.example.cookingapp.model.IngredientInRecipeKey;
-import com.example.cookingapp.model.Recipe;
+import com.example.cookingapp.model.*;
+import com.example.cookingapp.repositories.AmountRepository;
 import com.example.cookingapp.repositories.IngredientInRecipeRepository;
 import com.example.cookingapp.repositories.RecipeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -23,34 +23,48 @@ class RecipeController {
 
     private final RecipeRepository recipeRepository;
     private final IngredientInRecipeRepository ingredientInRecipeRepository;
+    private final AmountRepository amountRepository;
 
-    RecipeController(final RecipeRepository recipeRepository, final IngredientInRecipeRepository ingredientInRecipeRepository) {
+    private static final Logger logger = LoggerFactory.getLogger(RecipeController.class);
+
+    RecipeController(final RecipeRepository recipeRepository, final IngredientInRecipeRepository ingredientInRecipeRepository, final AmountRepository amountRepository) {
         this.recipeRepository = recipeRepository;
         this.ingredientInRecipeRepository = ingredientInRecipeRepository;
+        this.amountRepository = amountRepository;
     }
 
     @GetMapping
-    ResponseEntity<List<Recipe>> readAllRecipes(){
+    ResponseEntity<List<Recipe>> readAllRecipes() {
         return ResponseEntity.ok(recipeRepository.findAll());
     }
 
     @GetMapping("/{id}")
-    ResponseEntity<Recipe> readRecipe(@PathVariable int id){
+    ResponseEntity<Recipe> readRecipe(@PathVariable int id) {
         return recipeRepository.findById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    ResponseEntity<Recipe> addRecipe(@RequestBody @Valid Recipe toCreate){
+    ResponseEntity<Recipe> addRecipe(@RequestBody @Valid Recipe toCreate) {
         Recipe result = recipeRepository.save(toCreate);
         return ResponseEntity.created(URI.create("/" + result.getId())).body(result);
     }
 
-    @PostMapping(value = "/{id}",consumes = MediaType.APPLICATION_JSON_VALUE)
-    ResponseEntity<IngredientInRecipe> addIngredientToRecipe(@PathVariable(value = "id") final int recipeId,
-                                            @RequestBody @Valid Ingredient ingredient){
-        // TODO
+    @Transactional
+    @PostMapping(value = "/{recipeId}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity<IngredientInRecipe> addIngredientToRecipe(@PathVariable(value = "recipeId") final int recipeId,
+                                                             @RequestParam int ingredientId,
+                                                             @RequestParam String type,
+                                                             @RequestParam int number) {
+        Amount amount = new Amount(AmountType.valueOf(type),number);
+        amountRepository.save(amount);
+        IngredientInRecipeKey id = new IngredientInRecipeKey(recipeId, ingredientId);
+        if (ingredientInRecipeRepository.existsById(id)){
+            logger.warn("Such ingredientInRecipe already exists!");
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        ingredientInRecipeRepository.addIngredientToRecipe(ingredientId, recipeId, amount.getId());
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
