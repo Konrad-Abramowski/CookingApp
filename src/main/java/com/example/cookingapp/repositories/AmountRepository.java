ebfe34c2219d05a7acdcd773c68e705eb1d06926
Konrad-Abramowski <abramowski.konrad@gmail.com>
2020-08-18 16:22:47 +0200

    Override deleteById method

--- src/main/java/com/example/cookingapp/repositories/AmountRepository.java
+++ src/main/java/com/example/cookingapp/repositories/AmountRepository.java
@@ -2,8 +2,18 @@ package com.example.cookingapp.repositories;
 
 import com.example.cookingapp.model.Amount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface AmountRepository extends JpaRepository<Amount, Integer> {
+
+    @Override
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true, value = "delete from AMOUNT " +
+            "where AMOUNT.ID = :id")
+    void deleteById(Integer id);
 }
