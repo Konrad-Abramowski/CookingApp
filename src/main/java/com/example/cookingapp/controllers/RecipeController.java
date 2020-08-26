349651228bde4c71fc866b559728120ce0fafece
Konrad-Abramowski <abramowski.konrad@gmail.com>
2020-08-26 17:19:18 +0200

    rename 'left' variable to 'missing_ingredients'

--- src/main/java/com/example/cookingapp/controllers/RecipeController.java
+++ src/main/java/com/example/cookingapp/controllers/RecipeController.java
@@ -4,8 +4,13 @@ import com.example.cookingapp.model.*;
 import com.example.cookingapp.repositories.AmountRepository;
 import com.example.cookingapp.repositories.IngredientInRecipeRepository;
 import com.example.cookingapp.repositories.RecipeRepository;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -14,6 +19,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
+import javax.print.attribute.standard.Media;
 import javax.validation.Valid;
 import java.lang.reflect.Array;
 import java.net.URI;
@@ -62,26 +68,23 @@ class RecipeController {
         int resultRecipeIds = 0;
         List<List<Map<String, Object>>> result = new ArrayList<List<Map<String, Object>>>();
         int counter = 0;
-        for (int recipeId: recipeIds) {
+        for (int recipeId : recipeIds) {
             List<Integer> ingredientsForRecipe = ingredientInRecipeRepository.findIngredientIdsByRecipeId(recipeId);
-            for (int ingredientId: ingredientsForRecipe) {
-                for(int ownedIngredient: ownedIngredientIds){
-                    if(ownedIngredient == ingredientId){
+            for (int ingredientId : ingredientsForRecipe) {
+                for (int ownedIngredient : ownedIngredientIds) {
+                    if (ownedIngredient == ingredientId) {
                         resultRecipeIds++;
                     }
                 }
-
             }
             result.add(ingredientInRecipeRepository.showRecipeInfo(recipeId));
             Map<String, Object> toAdd = new HashMap<>();
-            toAdd.put("left", ingredientsForRecipe.size() - resultRecipeIds);
+            toAdd.put("MISSING_INGREDIENTS", ingredientsForRecipe.size() - resultRecipeIds);
             result.get(counter).add(toAdd);
             resultRecipeIds = 0;
             counter++;
         }
-
-
-        return  ResponseEntity.ok(result);
+        return ResponseEntity.ok(result);
     }
 
     @Transactional
