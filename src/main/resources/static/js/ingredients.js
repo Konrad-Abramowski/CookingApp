00b3ac5fb41b88cd05b65bf50c1d0fba550e565b
Konrad-Abramowski <abramowski.konrad@gmail.com>
2020-09-15 23:49:34 +0200

    Add 'Action' column

--- src/main/resources/static/js/ingredients.js
+++ src/main/resources/static/js/ingredients.js
@@ -15,12 +15,26 @@ fetch('http://localhost:8080/ingredients')
             thElement.scope = "row";
             thElement.appendChild(thText);
 
-            let nameTdElement = document.createElement("td");
-            let nameTdText = document.createTextNode(dataArray[i].name);
-            nameTdElement.appendChild(nameTdText);
+            let nameThElement = document.createElement("th");
+            let nameThText = document.createTextNode(dataArray[i].name);
+            nameThElement.appendChild(nameThText);
+
+            let actionThElement = document.createElement("th");
+
+            let actionDeleteAElement = document.createElement("a");
+            actionDeleteAElement.className = "badge badge-danger p-2 mr-1";
+            actionDeleteAElement.text = "Delete";
+            actionThElement.appendChild(actionDeleteAElement);
+
+            let actionUpdateAElement = document.createElement("a");
+            actionUpdateAElement.className = "badge badge-success p-2 mr-1";
+            actionUpdateAElement.text = "Edit";
+            actionThElement.appendChild(actionUpdateAElement);
+
 
             trElement.appendChild(thElement);
-            trElement.appendChild(nameTdElement);
+            trElement.appendChild(nameThElement);
+            trElement.appendChild(actionThElement);
             document.getElementById("ingredientTableBody").appendChild(trElement);
         }
     })
