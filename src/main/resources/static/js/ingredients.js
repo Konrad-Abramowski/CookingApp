8b454be559e6b43f4fd58ce1de140f5b7f2da2e6
Konrad-Abramowski <abramowski.konrad@gmail.com>
2020-09-22 23:03:38 +0200

    Refactor main method

--- src/main/resources/static/js/ingredients.js
+++ src/main/resources/static/js/ingredients.js
@@ -1,44 +1,52 @@
-fetch('http://localhost:8080/ingredients')
-    .then(result => result.json())
-    .then(data => {
-        let dataArray = [];
-        for (let i in data) {
-            dataArray.push(data[i]);
-        }
-        console.log(dataArray);
-        let counter = 1;
-        for (let i in dataArray) {
-            let trElement = document.createElement("tr");
+document.addEventListener("DOMContentLoaded", showIngredients);
 
-            let tdElement = document.createElement("td");
-            let tdText = document.createTextNode(counter++);
-            tdElement.appendChild(tdText);
+async function showIngredients() {
+    let result = [];
 
-            let nameTdElement = document.createElement("td");
-            let nameTdText = document.createTextNode(dataArray[i].name);
-            nameTdElement.appendChild(nameTdText);
+    result = await getIngredients();
 
-            let actionTdElement = document.createElement("td");
+    let dataArray = [];
+    for (let i in result) {
+        dataArray.push(result[i]);
+    }
 
-            let actionDeleteAElement = document.createElement("a");
-            actionDeleteAElement.className = "badge badge-danger p-2 mr-1";
-            actionDeleteAElement.text = "Delete";
-            actionTdElement.appendChild(actionDeleteAElement);
+    let counter = 1;
+    for (let i in dataArray) {
+        let trElement = document.createElement("tr");
 
-            let actionPElement = document.createElement("p");
-            actionPElement.textContent = "|";
-            actionPElement.className = "d-inline font-weight-bold";
-            actionTdElement.appendChild(actionPElement);
+        let tdElement = document.createElement("td");
+        let tdText = document.createTextNode(counter++);
+        tdElement.appendChild(tdText);
 
-            let actionUpdateAElement = document.createElement("a");
-            actionUpdateAElement.className = "badge badge-success p-2 ml-1";
-            actionUpdateAElement.text = "Edit";
-            actionTdElement.appendChild(actionUpdateAElement);
+        let nameTdElement = document.createElement("td");
+        let nameTdText = document.createTextNode(dataArray[i].name);
+        nameTdElement.appendChild(nameTdText);
 
+        let actionTdElement = document.createElement("td");
 
-            trElement.appendChild(tdElement);
-            trElement.appendChild(nameTdElement);
-            trElement.appendChild(actionTdElement);
-            document.getElementById("ingredientTableBody").appendChild(trElement);
+        let actionDeleteButtonElement = document.createElement("button");
+        actionDeleteButtonElement.className = "btn btn-danger mr-1";
+        actionDeleteButtonElement.textContent = "Delete";
+        actionTdElement.appendChild(actionDeleteButtonElement);
+
+        let actionUpdateAElement = document.createElement("button");
+        actionUpdateAElement.className = "btn btn-success mr-1";
+        actionUpdateAElement.textContent = "Edit";
+        actionTdElement.appendChild(actionUpdateAElement);
+
+        trElement.appendChild(tdElement);
+        trElement.appendChild(nameTdElement);
+        trElement.appendChild(actionTdElement);
+        document.getElementById("ingredientTableBody").appendChild(trElement);
+    }
+}
+
+async function getIngredients() {
+    const response = await fetch('http://localhost:8080/ingredients', {
+        'method': 'GET',
+        'headers': {
+            'Content-Type': 'application/json'
         }
     })
+    return response.json();
+}
